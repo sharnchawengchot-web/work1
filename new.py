05e0f0c6e3cbcfcd21d0c71bd9642d2278675f70
sharnchawengchot-web <sharnchawengchot@gmail.com>
2025-12-19 15:14:03 +0700

    Update new.py

--- new.py
+++ new.py
@@ -8,12 +8,11 @@ st.write(f"hello{x}")
 df = pd.read_csv('sustainable_waste_management_dataset_2024.csv')
 st.write(df)
 
-from numpy import *
 # st.table(df)
 
 
 
-'''
+
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
@@ -21,7 +20,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-url = '/sustainable_waste_management_dataset_2024.csv'
+url = 'sustainable_waste_management_dataset_2024.csv'
 df = pd.read_csv(url, parse_dates=['date'])
 
 df.head()
@@ -37,6 +36,25 @@ df_combined.dropna(inplace=True)
 X = df_combined[selected_features]
 y = df_combined['recyclable_kg']
 
-print(f"Shape of X: {X.shape}")
-print(f"Shape of y: {y.shape}")
-'''
+st.write("Shape of X:", X.shape)
+st.write("Shape of y:", y.shape)
+
+# แบ่ง dataframe เป็น 2 ส่วนหลัก ๆ โดยมีส่วนที่ใช้สำหรับฝึกโมเดลและทดสอบโมเดล
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = LinearRegression()
+model.fit(X_train, Y_train)
+Y_pred = model.predict(X_test)
+
+print("MSE: ", mean_squared_error(Y_test, Y_pred))
+print("R squared: ", r2_score(Y_test, Y_pred))
+
+# พล็อตกราฟเปรียบเทียบ
+plt.figure(figsize=(10, 6))
+plt.scatter(Y_test, Y_pred, alpha=0.7)
+plt.plot([y.min(), y.max()], [y.min(), y.max()], '--', color='red', lw=2, label='Perfect Prediction Line')
+plt.xlabel('Xcor')
+plt.ylabel('Ycor')
+plt.title('Predict')
+plt.legend()
+plt.grid(True)
+st.pyplot(plt)
